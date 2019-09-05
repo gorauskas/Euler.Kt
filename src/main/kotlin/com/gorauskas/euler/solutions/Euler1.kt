0721e80e92d0f4096b9e12296c8f1ddea5b92d9a
Jonas Gorauskas <jgorauskas@gmail.com>
2019-09-04 20:26:39 -0800

    adding problem 1 solution

---
+++ src/main/kotlin/com/gorauskas/euler/solutions/Euler1.kt
@@ -0,0 +1,26 @@
+package com.gorauskas.euler.solutions
+
+import com.gorauskas.euler.EulerInterface as IEuler
+
+class Euler1 : IEuler {
+
+    override fun solve(): Double {
+        return (1..999).filter { it % 3 == 0 || it % 5 == 0 }.sum().toDouble()
+    }
+
+    override val problem = """
+Project Euler Problem 1:
+
+   If we list all the natural numbers below 10 that are
+   multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of
+   these multiples is 23.
+
+   Find the sum of all the multiples of 3 or 5 below 1000.
+""".trimIndent()
+
+    override val answer = """
+
+The sum of all the multiples of 3 or 5 below 1000 is: ${solve()}
+""".trimIndent()
+
+}

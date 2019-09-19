fbb376ef609080830bbc1b48ef7ad2102676bdef
Jonas Gorauskas <jgorauskas@gmail.com>
2019-09-18 22:47:32 -0700

    adding problem 3 test

---
+++ src/test/kotlin/com/gorauskas/euler/Euler3Test.kt
@@ -0,0 +1,17 @@
+package com.gorauskas.euler
+
+import com.gorauskas.euler.solutions.Euler3
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Euler3Test {
+
+    @Test
+    fun testSolve() {
+
+        val e = Euler3()
+        Assertions.assertEquals(6857.0, e.solve())
+
+    }
+
+}

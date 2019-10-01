f3eb26a2d7d64d472543070e1ec3eba97e6a86b3
Jonas Gorauskas <jgorauskas@gmail.com>
2019-09-30 19:01:01 -0800

    adding problem 7 test

---
+++ src/test/kotlin/com/gorauskas/euler/Euler7Test.kt
@@ -0,0 +1,15 @@
+package com.gorauskas.euler
+
+import com.gorauskas.euler.solutions.Euler7
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Euler7Test {
+
+    @Test
+    fun testSolve() {
+        val e = Euler7()
+        Assertions.assertEquals(104743.0, e.solve())
+    }
+
+}

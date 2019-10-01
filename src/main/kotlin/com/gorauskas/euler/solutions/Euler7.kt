b09cdfc291f8b15145d1b1a91a6402976f367f85
Jonas Gorauskas <jgorauskas@gmail.com>
2019-09-30 19:25:01 -0800

    adding problem 7 solution

---
+++ src/main/kotlin/com/gorauskas/euler/solutions/Euler7.kt
@@ -0,0 +1,26 @@
+package com.gorauskas.euler.solutions
+
+import com.gorauskas.euler.sequences.primeSequence
+import com.gorauskas.euler.EulerInterface as IEuler
+
+class Euler7 : IEuler {
+
+    override fun solve(): Double {
+        return primeSequence(105000).toList().get(10000).toDouble()
+    }
+
+    override val problem = """
+    Project Euler Problem 7:
+    
+        By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+        we can see that the 6th prime is 13.
+    
+        What is the 10001st prime number?
+    """.trimIndent()
+
+    override val answer = """
+        
+    Prime 10001 is ${solve().toLong()}
+    """.trimIndent()
+
+}

72e77f6dcf88353992490e064700d095ef57a080
Jonas Gorauskas <jgorauskas@gmail.com>
2019-09-30 19:11:01 -0800

    adding problem 6 test

---
+++ src/test/kotlin/com/gorauskas/euler/Euler6Test.kt
@@ -0,0 +1,17 @@
+package com.gorauskas.euler
+
+import com.gorauskas.euler.solutions.Euler6
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Euler6Test {
+
+    @Test
+    fun testSolve() {
+
+        val e = Euler6()
+        Assertions.assertEquals(25164150.0, e.solve())
+
+    }
+
+}

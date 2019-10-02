2619ee59e0eaeea63ac9e37d514904df32223e4e
Jonas Gorauskas <jgorauskas@gmail.com>
2019-10-01 19:01:01 -0800

    adding problem 8 test

---
+++ src/test/kotlin/com/gorauskas/euler/Euler8Test.kt
@@ -0,0 +1,15 @@
+package com.gorauskas.euler
+
+import com.gorauskas.euler.solutions.Euler8
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Euler8Test {
+
+    @Test
+    fun testSolve() {
+        val e = Euler8()
+        Assertions.assertEquals(23514624000.0, e.solve())
+    }
+
+}

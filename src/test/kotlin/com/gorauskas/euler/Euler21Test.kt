4cdcb919204ed682ef3df3c40af27be93724de7a
Jonas Gorauskas <jgorauskas@gmail.com>
2020-07-06 00:48:41 -0700

    adding problem 21 test + solution

---
+++ src/test/kotlin/com/gorauskas/euler/Euler21Test.kt
@@ -0,0 +1,15 @@
+package com.gorauskas.euler
+
+import com.gorauskas.euler.solutions.Euler21
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Euler21Test {
+
+    @Test
+    fun testSolve() {
+        val e = Euler21()
+        Assertions.assertEquals(31626.0, e.solve())
+    }
+
+}

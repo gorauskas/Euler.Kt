2a8d501328d38a15830273457a175082fca9f77b
Jonas Gorauskas <jgorauskas@gmail.com>
2020-07-25 13:07:42 -0800

    adding problem 20 test + solution + resources

---
+++ src/test/kotlin/com/gorauskas/euler/Euler22Test.kt
@@ -0,0 +1,15 @@
+package com.gorauskas.euler
+
+import com.gorauskas.euler.solutions.Euler22
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Euler22Test {
+
+    @Test
+    fun testSolve() {
+        val e = Euler22()
+        Assertions.assertEquals(871198282.0, e.solve())
+    }
+
+}

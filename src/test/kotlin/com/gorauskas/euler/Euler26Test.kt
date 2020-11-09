59e83afaeb8731d0d875f1e66733b632a0df4ba0
Jonas Gorauskas <jgorauskas@gmail.com>
2020-11-09 01:40:56 -0800

    adding problem 26 test + solution

---
+++ src/test/kotlin/com/gorauskas/euler/Euler26Test.kt
@@ -0,0 +1,15 @@
+package com.gorauskas.euler
+
+import com.gorauskas.euler.solutions.Euler26
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Euler26Test {
+
+    @Test
+    fun testSolve() {
+        val e = Euler26()
+        Assertions.assertEquals(983.0, e.solve())
+    }
+
+}

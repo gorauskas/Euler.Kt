9b08d486a70c1b2d019fd01a9bae812ce3c0845a
Jonas Gorauskas <jgorauskas@gmail.com>
2020-11-09 16:02:17 -0800

    adding problem 27 test + solution

---
+++ src/test/kotlin/com/gorauskas/euler/Euler27Test.kt
@@ -0,0 +1,15 @@
+package com.gorauskas.euler
+
+import com.gorauskas.euler.solutions.Euler27
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Euler27Test {
+
+    @Test
+    fun testSolve() {
+        val e = Euler27()
+        Assertions.assertEquals(-59231.0, e.solve())
+    }
+
+}

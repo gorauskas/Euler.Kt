95c83274acf88a2fd0793f09749d041699b524c8
Jonas Gorauskas <jgorauskas@gmail.com>
2022-08-06 17:15:37 -0700

    use sequence scopes + yield

--- src/main/kotlin/com/gorauskas/euler/Sequences.kt
+++ src/main/kotlin/com/gorauskas/euler/Sequences.kt
@@ -2,23 +2,22 @@ package com.gorauskas.euler.sequences
 
 import com.gorauskas.euler.extensions.isPrime
 
-fun fibonacciSequence(): Sequence<Long> {
-
-    return generateSequence(Pair(0, 1), {
-        Pair(it.second, it.first + it.second)
-    }).map {
-        it.first.toLong()
+fun fibonacciSequence(): Sequence<Long> =
+    sequence {
+        var seed = Pair(0L, 1L)
+        while (true) {
+            yield(seed.first)
+            seed = Pair(seed.second, seed.first + seed.second)
+        }
     }
 
-}
-
 fun primeSequence(): Sequence<Long> {
-
     var i = 0L
-
-    return generateSequence { i++ }
+    return sequence {
+        generateSequence { i++ }
             .filter { n -> n.isPrime() }
-
+            .forEach { yield(it) }
+    }
 }
 
 fun primeSequence(max: Int): Sequence<Long> {
@@ -27,7 +26,12 @@ fun primeSequence(max: Int): Sequence<Long> {
 
 fun triangleSequence(): Sequence<Long> {
     val i = 1L;
-    return generateSequence(i, { it + 1 }).map { n -> (n * n + n) / 2 }
+    return sequence {
+        generateSequence(i, { it + 1 })
+            .forEach { n ->
+                yield((n * n + n) / 2)
+            }
+    }
 }
 
 fun triangleSequence(max: Int): Sequence<Long> {

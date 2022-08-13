e9a321db6c7dfe787419d242c5dd4393778c04ad
Jonas Gorauskas <jgorauskas@gmail.com>
2022-08-13 02:27:28 -0700

    renamed interface

--- src/main/kotlin/com/gorauskas/euler/EulerSolution.kt
+++ src/main/kotlin/com/gorauskas/euler/EulerSolution.kt
@@ -1,9 +1,9 @@
 package com.gorauskas.euler
 
-interface EulerInterface {
+interface EulerSolution {
     fun solve(): Double
     val problem: String
     val answer: String
 }
 
-typealias IEuler = EulerInterface
+typealias IEuler = EulerSolution

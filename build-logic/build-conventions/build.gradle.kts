1d8906beef3b15e67784115ecee568e1468b403d
Jonas Gorauskas <jgorauskas@gmail.com>
2023-11-23 21:26:36 -0700

    add dependencies for convention plugin

--- build-logic/build-conventions/build.gradle.kts
+++ build-logic/build-conventions/build.gradle.kts
@@ -1,9 +1,28 @@
 plugins {
     `kotlin-dsl`
-    embeddedKotlin("jvm")
 }
 
 repositories {
     gradlePluginPortal()
     mavenCentral()
+}
+
+dependencies {
+    listOf(
+        buildLibs.build.kotlin.jvm,
+        buildLibs.build.kotlin.api,
+    ).forEach {
+        implementation(it)
+    }
+
+    listOf(
+        buildLibs.build.detekt,
+        buildLibs.build.spotless,
+        buildLibs.build.dependency.analysis,
+        buildLibs.build.owasp.dependencycheck,
+        buildLibs.build.versions,
+        buildLibs.build.tasktree,
+    ).forEach {
+        api(it)
+    }
 }

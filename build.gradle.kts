208c4faca57b2218081c9f249be5aa36eebe6e2a
Jonas Gorauskas <jgorauskas@gmail.com>
2023-11-23 21:39:15 -0700

    cleanup main build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
-import io.gitlab.arturbosch.detekt.Detekt
+import com.gradle.scan.agent.serialization.scan.serializer.kryo.it
 
 plugins {
+    // id("local.build-conventions")
     idea
     java
     application
     kotlin("jvm") version libs.versions.kotlin.lang.get()
-    alias(libs.plugins.test.logger)
-    alias(libs.plugins.detekt)
+    id("local.build-conventions")
+    id("local.code-analysis")
+    id("local.dependency-analysis")
+    id("local.testing")
 }
 
 group = "com.gorauskas"
@@ -39,76 +42,4 @@ dependencies {
     }
 
     detektPlugins(libs.detekt.formatting)
-}
-
-sourceSets {
-    main {
-        java {
-            srcDirs("$projectDir/src/main/kotlin/")
-        }
-    }
-    test {
-        java {
-            srcDirs("$projectDir/src/test/kotlin/")
-        }
-    }
-}
-
-tasks {
-    jar {
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        archiveBaseName.set("euler")
-        manifest {
-            attributes["Main-Class"] = "com.gorauskas.euler.AppKt"
-        }
-        from(sourceSets.main.get().output)
-        dependsOn(configurations.runtimeClasspath)
-        from({
-            configurations.runtimeClasspath.get().map {
-                if (it.isDirectory)
-                    it
-                else
-                    zipTree(it)
-            }
-        })
-    }
-}
-
-tasks.compileKotlin {
-    kotlinOptions.jvmTarget = "17"
-}
-
-tasks.compileTestKotlin {
-    kotlinOptions.jvmTarget = "17"
-}
-
-tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
-tasks.withType<Detekt>().configureEach {
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-        txt.required.set(false)
-        sarif.required.set(false)
-        md.required.set(false)
-    }
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-detekt {
-    allRules = true
-    parallel = true
-    buildUponDefaultConfig = true
-    toolVersion = libs.versions.detekt.get()
-    config.from(files("${rootDir.path}/detekt.yml"))
-    source.from(files("src/main/kotlin", "src/test/kotlin"))
 }

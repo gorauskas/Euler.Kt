f97c7db9f80dca667e95186e9a4e6cbc6addad93
Jonas Gorauskas <jgorauskas@gmail.com>
2023-11-23 23:52:26 -0700

    update settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,21 @@
-rootProject.name = "Euler.Kt"
+@file:Suppress("UnstableApiUsage")
+import org.gradle.api.internal.FeaturePreviews.Feature.TYPESAFE_PROJECT_ACCESSORS
+
+rootProject.name = "EulerKt"
+
+enableFeaturePreview(TYPESAFE_PROJECT_ACCESSORS.name)
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
 pluginManagement {
     repositories {
+        mavenLocal()
         gradlePluginPortal()
-        mavenCentral()
     }
     includeBuild("build-logic")
 }
